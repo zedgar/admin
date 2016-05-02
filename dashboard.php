94a6d4c11df503bd8ea986a6ddb52f8569fe3e2c
zedgar <eugenekz.tech@gmail.com>
2016-05-02 02:48:42 +0200

    added admin repository
    This the dashboard and administration panels

---
+++ dashboard.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" class="ie"lang="en-US">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7"  class="ie"lang="en-US">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8"  class="ie"lang="en-US">
+<![endif]-->
+<!--[if IE 9]>
+<html id="ie9"  class="ie"lang="en-US">
+<![endif]-->
+<!--[if gt IE 9]>
+<html class="ie"lang="en-US">
+<![endif]-->
+
+<!-- This doesn't work but i prefer to leave it here... maybe in the future the MS will support it... i hope... -->
+<!--[if IE 10]>
+<html id="ie10"  class="ie"lang="en-US">
+<![endif]-->
+
+
+<!--[if !IE]>
+<html lang="en-US">
+<![endif]-->
+
+<!-- START HEAD -->
+<?php session_start();?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Rexis | Dashboard </title>
+        <link rel="stylesheet" href="../css/bootstrap.css">
+        <link rel="stylesheet" href="../css/responsive.css">
+        <link rel="stylesheet" href="../css/bootstrap-responsive.css">
+        <link rel="stylesheet" href="custom.css">
+        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    </head>
+    <body>
+        <?php include_once 'headerreg.php';?>
+        
+        
+        <script src="http:code.jquery.com/jquery-1.9.0.min.js"></script>
+        <script src="../js/bootstrap.js"></script>
+        <script src="../js/responsive.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+    </body>
+</html>
+
+
---
+++ headerreg.php
@@ -0,0 +1,90 @@
+<!-- START HEADER -->
+<div id="header" class="group margin-bottom">
+
+
+
+    <div class="group container">
+            <div class="row" id="logo-headersidebar-container">
+                <!-- START LOGO -->
+                <div id="logo" class="span6 group">
+                    <a id="logo-img" href="index.php" title="Rexis">
+                        <img src="../images/rexis_new_logo.png" title="Rexis" alt="Rexis Student Information System" />
+                    </a>
+                   
+                </div>
+               
+                <!-- END LOGO -->
+
+                <!-- START HEADER SIDEBAR -->
+                <div id="header-sidebar" class="span6 group">
+                    <div class="widget-first widget header-text-image">
+                        <!--<div class="text-image" style="text-align:left">
+                            <img src="images/footer/author-footer.png" alt="USER" />
+                        </div>
+
+                        <div class="text-content">
+                            <h3>USER:</h3>
+                            <p></p>
+                        </div>-->
+                    </div>   
+                </div>                
+            </div>
+             
+    </div>
+     <hr>
+        <!-- START TOP BAR -->
+        <div class="container"></div>
+            <div id="topbar" class="nav span3 well">
+                <li class="nav-header" >
+                     <a data-toggle="collapse" href="#reports-menu" aria-expanded="false" aria-controls="reports-menu"> <b>reports</b></a>
+                     <div class="collapse" id="reports-menu">
+                        <div class="card card-block">
+                            <ul class="list-group" id="reports-menu">
+                                <li class="list-group-item"><a href=""><i>Student Enrollment</i></a></li>
+                                <li class="list-group-item"><a href=""><i>Cohort Exam Reports</i></a></li>
+                                <li class="list-group-item"><a href=""><i>Prepare Transcript</i></a></li>
+                                <li class="list-group-item"><a href=""><i>Cohort Transcripts</i></a></li>
+                            </ul>
+                        </div>
+                     </div>
+                </li>
+                <hr>
+
+                 <li class="nav-header" >
+                     <a data-toggle="collapse" href="#access-control" aria-expanded="false" aria-controls="access-control"> <b>Access Control</b></a>
+                     <div class="collapse" id="access-control">
+                        <div class="card card-block">
+                            <ul class="list-group" id="access-control">
+                                <li class="list-group-item"><a href=""><i>Deny Access</i></a></li>
+                                <li class="list-group-item"><a href=""><i>Grant Access</i></a></li>
+                                <li class="list-group-item"><a href=""><i>Account Reset</i></a></li>
+                                <li class="list-group-item"><a href=""><i>Superusers Access</i></a></li>
+                            </ul>
+                        </div>
+                     </div>
+                </li>
+                <hr>
+                <li class="nav-header" >
+                     <a data-toggle="collapse" href="#paylist" aria-expanded="false" aria-controls="paylist"> <b>Payments</b></a>
+                     <div class="collapse" id="paylist">
+                        <div class="card card-block">
+                            <ul class="list-group" id="paylist">
+                                <li class="list-group-item"><a href=""><i>Tuition</i></a></li>
+                                <li class="list-group-item"><a href=""><i>Accommodation</i></a></li>
+                                <li class="list-group-item"><a href=""><i>Technology</i></a></li>
+                                <li class="list-group-item"><a href=""><i>Sports</i></a></li>
+                                <li class="list-group-item"><a href=""><i>medical</i></a></li>
+                            </ul>
+                        </div>
+                     </div>
+                </li>
+              <?php //include 'navbar2.php'; ?>
+            </div>
+        </div>
+    <!-- END TOP BAR -->
+
+    <div class="top-border span12">
+    </div>
+        
+</div>
+<!-- END HEADER -->
